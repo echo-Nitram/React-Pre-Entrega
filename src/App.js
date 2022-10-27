43a000971eac970ae395c2b17db1ca8388d8e947
echo-Nitram <55297354+echo-Nitram@users.noreply.github.com>
2022-10-26 21:10:52 -0300

    Primera Entrega

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import ResponsiveAppBar from './Component/navBar';
+import ItemListContainer from './Component/itemListContainer';
+
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ResponsiveAppBar />
+      <ItemListContainer greeting={"Bienvenido"} />
+    </>
+  )
 }
 
 export default App;
